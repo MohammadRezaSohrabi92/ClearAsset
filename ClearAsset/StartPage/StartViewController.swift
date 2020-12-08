a9d7bd7592d1e74a6fe6ab8d368e935acdad7503
mohammadreza <sohrabi.developer@gmail.com>
2020-12-08 13:10:42 +0330

    first commit

---
+++ ClearAsset/StartPage/StartViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StartViewController.swift
+//  ClearAsset
+//
+//  Created by Peyk Office on 07/12/2020.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    @IBOutlet weak var btnGetStarted: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnGetStartedAction(_ sender: Any) {
+        //code
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ClearAsset/Utility/Utility.swift
@@ -0,0 +1,19 @@
+//
+//  Utility.swift
+//  ClearAsset
+//
+//  Created by Peyk Office on 08/12/2020.
+//
+
+import Foundation
+import UIKit
+import PKHUD
+
+class Utility {
+    
+}
+
+
+
+
+
