3aeca2be67e6206298d96010d37f040ed576cf8f
mohammadreza <sohrabi.developer@gmail.com>
2020-12-20 10:46:37 +0330

    register completed

---
+++ ClearAsset/UI/CustomUI/CustomScrollView.swift
@@ -0,0 +1,25 @@
+//
+//  CustomScrollView.swift
+//  ClearAsset
+//
+//  Created by Peyk Office on 20/12/2020.
+//
+
+import Foundation
+import UIKit
+
+class CustomScrollView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        //
+    }
+    
+    
+    
+}
